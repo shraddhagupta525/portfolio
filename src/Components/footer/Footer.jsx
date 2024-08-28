693ca94c7a27bf9c145a24d0874b3426ce2f6681
shraddhagupta <shraddhagupta8216@gmail.com>
2024-08-29 00:13:15 +0530

    footer

--- src/Components/footer/Footer.jsx
+++ src/Components/footer/Footer.jsx
@@ -6,20 +6,18 @@ import { FaGithub } from "react-icons/fa";
 import { SiLeetcode } from "react-icons/si";
 import { FaInstagram } from "react-icons/fa6";
 
-
 export default function Footer() {
   return (
     <footer>
       <div className="connect">
         <h1 className="connect-txt">Let's Connect there </h1>
-        <button type="submit" className="connect-btn">
-          <a
-            href="https://www.linkedin.com/in/shraddha-gupta-7a44a1237/"
-            target="_blank"
-          >
-            <span className="inner-connect">Connect</span>
-          </a>
-        </button>
+        <a
+          href="https://www.linkedin.com/in/shraddha-gupta-7a44a1237/"
+          target="_blank"
+          className="btn connect-btn"
+        >
+          <span className="inner-connect">Connect</span>
+        </a>
       </div>
       <hr className="horizontal-line" />
       <div className="details">
@@ -32,20 +30,51 @@ export default function Footer() {
             LeetCode challenges. Currently pursuing B.Tech in Computer Science
             at Lovely Professional University.
           </p>
-          <div className="social-links">
-            <a href="https://www.linkedin.com/in/shraddha-gupta-7a44a1237/">
-              <FaLinkedin />
-            </a>
-            <a href="https://github.com/shraddhagupta525"><FaGithub />
 
-            </a>
-            <a href="https://leetcode.com/u/shraddhagupta525/">
-              <SiLeetcode />
-            </a>
-            <a href="https://www.instagram.com/shraddhagupta_525/">
+        <div className="social-links">
+          <a href="https://www.linkedin.com/in/shraddha-gupta-7a44a1237/">
+            <FaLinkedin />
+          </a>
+          <a href="https://github.com/shraddhagupta525">
+            <FaGithub />
+          </a>
+          <a href="https://leetcode.com/u/shraddhagupta525/">
+            <SiLeetcode />
+          </a>
+          <a href="https://www.instagram.com/shraddhagupta_525/">
             <FaInstagram />
+          </a>
+        </div>
+          <div className="navigation">
+            <h2>Navigation</h2>
+            <ul className="menu">
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">About</a>
+              </li>
+              <li>
+                <a href="#">Projects</a>
+              </li>
+              <li>
+                <a href="#">Contact</a>
+              </li>
+            </ul>
+          </div>
+          <div className="contact">
+          <h2>Contact</h2>
 
-            </a>
+            <ul>
+              <li>
+                <a href="tel:9335121330">9335121330</a>
+              </li>
+              <li>
+                <a href="mailto:shraddhagupta8216@gmail.com">
+                  shraddhagupta8216@gmail.com
+                </a>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
